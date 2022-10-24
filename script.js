980d58b1983b5b0545732f561cde326b19f0dcd8
unknown <kibrish49@gmail.com>
2022-10-24 14:42:30 +0200

    Add create and remove books from the list

--- script.js
+++ script.js
@@ -1,58 +1,74 @@
+const addBtn = document.getElementById('add');
+// Declaring varibale for the books' container
+const  main = document.querySelector('main')
+const  deleteBtn= document.querySelectorAll('.remove');
+
+deleteBtn.forEach(function(btn){
+    btn.addEventListener('click',function(){
+        // deleteBook(1);
+        console.log("hello");
+    })
+});
+
 // Creating Array of objects(Book List)
 const books_list = [
-    { title : 'The Alchemist',
-     author : 'Paulo Coelho', 
+    { id:1,
+        title : 'The Alchemist',
+        author : 'Paulo Coelho', 
     },
 
-    { title : 'The Animal farm',
-      author : 'Paulo Jack',
+    { id:2,
+        title : 'The Animal farm',
+        author : 'Paulo Jack',
     }
 ]
 
+// Function to display the added book
+function displayBook(book) {
+        const bookMarkup= `<div class="book_list">
+        <p>${book.title}</p>
+        <p>${book.author}</p>
+        <button class="remove" id=${book.id}>Remove</button>
+        <hr>`;
+    main.innerHTML +=bookMarkup; 
 
-// Declaring varibale for the books' container
-let  main = document.querySelector('main')
-
-// Function to display the added books
-function displayBook() {
-    books_list.forEach(book => {
-    main.innerHTML += 
-    `<div class="book_list">
-    <p>${book.title}</p>
-    <p>${book.author}</p>
-    <button class="remove">Remove</button>
-    <hr>
-</div>`
-})
+ 
 }
-// displayBook(book);
+
+// recursively call displayBook(book);
+function displayBooks(){
+    books_list.forEach((book)=>{
+        displayBook(book);
+    });
+}
 
 
-let addBtn = document.getElementById('add');
-let forum = document.getElementsByClassName('forum');
-let book;
 addBtn.addEventListener('click', function() {
     let t = document.getElementById('title')
     let a = document.getElementById('author')
-    book = {title:t, author:a};
-    books_list.push(book);
-    console.log('hi');
+    const ids= books_list.length+1;
+    let book ={id:ids,title:t,author:a}
+    
+    createBook(ids,t,a);
+
+    displayBook(book);
 }
 );
-displayBook();
+
+function createBook(bId,bAuthor,bTitle){
+    const book={id: bId,title:bTitle,author:bAuthor};
+    books_list.push(book)
+}
 
 
 
-// let book = {title:'title', author:'author'};
-// books_list.push(book);
+function deleteBook(id){
+    let books_lists=books_list.filter(item=>{
+        return item.id!==id;
+    })
+    console.log(books_lists)
+}
 
+// deleteBook(3);
 
-
-
-
-
-
-
-
-
-
+window.addEventListener('load',displayBooks);
