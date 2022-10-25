020718888b8f635d5c2a1e4fc5e368e6996ca106
unknown <kibrish49@gmail.com>
2022-10-25 06:31:36 +0200

    Add remove book from the list

--- localStorage.js
+++ localStorage.js
@@ -1,25 +1,24 @@
 const bookList = {
-    title: '',
-    author: '',
-  };
-  
-  const titleInput = document.getElementById('title');
-  const authorInput = document.getElementById('author');
-  
-  titleInput.addEventListener('input', () => {
-    bookList.title = titleInput.value;
-    localStorage.setItem('data', JSON.stringify(bookList));
-  });
-  
-  authorInput.addEventListener('input', () => {
-    bookList.author = authorInput.value;
-    localStorage.setItem('data', JSON.stringify(bookList));
-  });
-  
-  if (localStorage.getItem('data')) {
-    const formValue = localStorage.getItem('data');
-    const formValueObj = JSON.parse(formValue);
-    titleInput.value = formValueObj.title;
-    authorInput.value = formValueObj.author;
-  }
-  
+  title: '',
+  author: '',
+};
+
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+
+titleInput.addEventListener('input', () => {
+  bookList.title = titleInput.value;
+  localStorage.setItem('data', JSON.stringify(bookList));
+});
+
+authorInput.addEventListener('input', () => {
+  bookList.author = authorInput.value;
+  localStorage.setItem('data', JSON.stringify(bookList));
+});
+
+if (localStorage.getItem('data')) {
+  const formValue = localStorage.getItem('data');
+  const formValueObj = JSON.parse(formValue);
+  titleInput.value = formValueObj.title;
+  authorInput.value = formValueObj.author;
+}
--- script.js
+++ script.js
@@ -1,68 +1,72 @@
 const addBtn = document.getElementById('add');
 // Declaring varibale for the books' container
-const  main = document.querySelector('main')
+const main = document.querySelector('main');
 
 // Creating Array of objects(Book List)
-let books_list = [
-    { id:1,
-        title : 'The Alchemist',
-        author : 'Paulo Coelho', 
-    },
+let booksList = [
+  {
+    id: 1,
+    title: 'The Alchemist',
+    author: 'Paulo Coelho',
+  },
 
-    { id:2,
-        title : 'The Animal farm',
-        author : 'Paulo Jack',
-    }
-]
+  {
+    id: 2,
+    title: 'The Animal farm',
+    author: 'Paulo Jack',
+  },
+];
+
+// Re-display the books list after deleted item
+function refresh() {
+  main.innerHTML = '';
+  displayBooks();
+}
+
+// Remove a book with id from the page book list
+function deleteBook(id) {
+  booksList = booksList.filter((item) => item.id !== parseInt(id));
+  refresh();
+}
 
 // Function to display the added book
 function displayBook(book) {
-        const bookMarkup= `<div class="book_list">
+  const bookMarkup = `<div class="book_list">
         <p>${book.title}</p>
         <p>${book.author}</p>
         <button class="remove" id=${book.id}>Remove</button>
         <hr>`;
-    main.innerHTML +=bookMarkup; 
-    let deleteBtn= document.querySelectorAll('.remove');
-    deleteBtn.forEach((btn)=>{
-    btn.addEventListener('click',function(){
-        deleteBook(btn.id);
-        console.log(btn.id);
-
-    })
-});
+  main.innerHTML += bookMarkup;
+  const deleteBtn = document.querySelectorAll('.remove');
+  deleteBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      deleteBook(btn.id);
+    });
+  });
 }
 
 // recursively call displayBook(book);
-async function displayBooks(){
-    books_list.forEach((book)=>{
-        displayBook(book);
-        
-    });
-}
-
-addBtn.addEventListener('click', function() {
-    let title = document.getElementById('title')
-    let author = document.getElementById('author')
-    let id= books_list.length+1;
-    let book = {
-        id: id,
-        title: title.value,
-        author: author.value,
-    }
+function displayBooks() {
+  booksList.forEach((book) => {
     displayBook(book);
-    books_list.push(book);
-});
-
-
-function deleteBook(id){
-    books_list = books_list.filter(item=>{
-    return item.id!==parseInt(id);
-    })
-    console.log(books_list)
-    displayBooks();
+  });
 }
 
-// deleteBook(3);
+// Add book to the list
+function addBook() {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const id = booksList.length + 1;
+  const book = {
+    id,
+    title: title.value,
+    author: author.value,
+  };
+  displayBook(book);
+  booksList.push(book);
+}
 
-window.addEventListener('load',displayBooks);
+// create book
+addBtn.addEventListener('click', addBook);
+
+window.addEventListener('load', displayBooks);
