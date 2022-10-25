5a22ee4112bf709a3125a3a3244c17ccfa104e84
unknown <kibrish49@gmail.com>
2022-10-25 22:27:38 +0200

    Update the refactoring add books from the form, deleting from the list and its syling

--- script.js
+++ script.js
@@ -1,63 +1,6 @@
 const addBtn = document.getElementById('add');
 // Declaring varibale for the books' container
 const listContainer = document.querySelector('.list_container');
-
-// // Creating Array of objects(Book List)
-// let booksList = JSON.parse(localStorage.getItem('books')) || [];
-
-// // Function to delete book from the list
-// function deleteBook(id) {
-//   booksList = booksList.filter((item) => item.id !== parseInt(id, 10));
-//   localStorage.setItem('books', JSON.stringify(booksList));
-// }
-
-// // Function to display the added book
-// function displayBook(book) {
-//   const bookMarkup = `<div class="book_list">
-//         <span class="book_text">${book.title} by ${book.author} </span>
-//         <button class="remove" id=${book.id}>Remove</button>
-//         </div>`;
-//   listContainer.innerHTML += bookMarkup;
-//   const deleteBtn = document.querySelectorAll('.remove');
-//   deleteBtn.forEach((btn) => {
-//     btn.addEventListener('click', () => {
-//       deleteBook(btn.id);
-//       btn.parentElement.remove();
-//     });
-//   });
-// }
-
-// // recursively call displayBook(book);
-// function displayBooks() {
-//   booksList.forEach((book) => {
-//     displayBook(book);
-//   });
-// }
-
-// // Function to add book to the list
-// addBtn.addEventListener('click', () => {
-//   const title = document.getElementById('title');
-//   const author = document.getElementById('author');
-//   if (title.value === '' || author.value === '') {
-//     window.alert('Please enter the book title and author');
-//   } else {
-//     const id = booksList.length + 1;
-//     const book = {
-//       id,
-//       title: title.value,
-//       author: author.value,
-//     };
-//     displayBook(book);
-//     booksList.push(book);
-//     localStorage.setItem('books', JSON.stringify(booksList));
-//   }
-// });
-
-// // Calling displayBooks() function on page load
-// window.addEventListener('load', displayBooks);
-
-// Class Approach
-
 const booksList = JSON.parse(localStorage.getItem('books')) || [];
 class Book {
   constructor(id, title, author) {
@@ -105,8 +48,12 @@ class Book {
       const deleteBtn = document.querySelectorAll('.remove');
       deleteBtn.forEach((btn) => {
         btn.addEventListener('click', () => {
-          Book.removeBook(btn.id);
-          btn.parentElement.remove();
+          if(Book.removeBook(btn.id)){
+            btn.parentElement.remove();
+          }
+          else{
+            btn.classList.add('disabled');
+          }
         });
       });
     });
@@ -123,7 +70,7 @@ addBtn.addEventListener('click', () => {
   if (title.value === '' || author.value === '') {
     window.alert('Please enter the book title and author');
   } else {
-    const b = new Book(id, title, author);
+    const b = new Book(id, title.value, author.value);
     Book.addBook(b);
   }
 });
